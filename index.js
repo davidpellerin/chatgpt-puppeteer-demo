6ca05097d6bd5b0dfbbb1862cba9e9b7aa088f26
David <david@davidpellerin.com>
2023-02-12 22:33:12 -0500

    adding dotenv

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import { ChatGPTAPIBrowser } from "chatgpt";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const api = new ChatGPTAPIBrowser({
   email: process.env.OPENAI_EMAIL,
   password: process.env.OPENAI_PASSWORD,
   isProAccount: true,
 });
+
 await api.initSession();
 
 const result = await api.sendMessage("Hello World!");
+
 console.log(result.response);
