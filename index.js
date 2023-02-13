e7d0f4041769fd8e9cc9a2fa5d4c7c7b5a3bb357
David <david@davidpellerin.com>
2023-02-13 07:09:53 -0500

    adding .env and cli args

--- index.js
+++ index.js
@@ -2,15 +2,32 @@ import { ChatGPTAPIBrowser } from "chatgpt";
 import * as dotenv from "dotenv";
 
 dotenv.config();
+const args = process.argv;
 
-const api = new ChatGPTAPIBrowser({
+const opts = {
   email: process.env.OPENAI_EMAIL,
   password: process.env.OPENAI_PASSWORD,
-  isProAccount: true,
-});
+  isProAccount: process.env.OPENAI_PRO,
+};
+
+const api = new ChatGPTAPIBrowser(opts);
+
+if (process.env.PROXY_HOST) {
+  api.proxyServer = `${process.env.PROXY_USERNAME}:${process.env.PROXY_PASSWORD}@${process.env.PROXY_HOST}:${process.env.PROXY_PORT}}`;
+}
+if (args.length <= 2) {
+  console.log("Error: No prompt passed in.");
+  console.log("");
+  console.log("Usage: node index.js <prompt>");
+  console.log("");
+  process.exit(1);
+}
 
 await api.initSession();
 
-const result = await api.sendMessage("Hello World!");
+const result = await api.sendMessage(args[2]);
 
-console.log(result.response);
+if (result) {
+  console.log(result.response);
+  process.exit(0);
+}
